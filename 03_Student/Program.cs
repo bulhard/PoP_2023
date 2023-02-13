acf5874826fe21475089df8e1182c0c44519be8f
bulhard <pravoslav.milenkov@gmail.com>
2023-02-13 18:51:40 +0200

    Student class

---
+++ 03_Student/Program.cs
@@ -0,0 +1,11 @@
+﻿using _03_Student;
+
+Student student1 = new Student();
+
+Student student2 = new Student("Ivan", "Petrov");
+
+Console.WriteLine(student1.FullName);
+
+Console.WriteLine(student2.FullName);
+
+Console.WriteLine(student2);
---
+++ 03_Student/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Student
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName { get => firstName; }
+
+        public string LastName { get => lastName; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{firstName} {lastName}";
+            }
+        }
+
+        public Course Course { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+
+        public string Email { 
+            get 
+            {
+                if (string.IsNullOrEmpty(email))
+                {
+                    return "n/a";
+                }
+                return email;
+
+            }
+            set => email = value; }
+
+        public string PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full Name: {FullName} E-mail: {Email} Phone: {PhoneNumber}...";
+        }
+    }
+}
