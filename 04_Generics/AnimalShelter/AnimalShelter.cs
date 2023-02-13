6c88452b75204a6ca10de7c594cca1afd323dcc4
bulhard <pravoslav.milenkov@gmail.com>
2023-02-13 19:48:29 +0200

    Generics

---
+++ 04_Generics/AnimalShelter/AnimalShelter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Generics.AnimalShelter
+{
+    public class AnimalShelter<T>
+    {
+        private int usedPlaces = 0;
+
+        public AnimalShelter(int capacity)
+        {
+            Places = new T[capacity];
+        }
+
+        public T[] Places { get; set; }
+
+        public void AddNew(T newAnimal)
+        {
+            Places[usedPlaces] = newAnimal;
+            usedPlaces++;
+        }
+    }
+}
---
+++ 04_Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using _04_Generics.AnimalShelter;
+
+
+AnimalShelter<Dog> shelter1 = new AnimalShelter<Dog>(10);
+
+Dog dog1 = new Dog();
+
+shelter1.AddNew(dog1);
+
+
+AnimalShelter<Cat> shelter2 = new AnimalShelter<Cat>(20);
+
+Cat cat1 = new Cat();
+
+shelter2.AddNew(cat1);
+
